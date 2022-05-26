01bfd8faf4724ffc929b0e0d234c8de8cfa1a754
sivakumar <sivakumar3b8@gmail.com>
2022-05-26 18:47:07 +0530

    adding the url path in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,8 @@ Rails.application.configure do
   # timestamps for the last write to the primary. The resolver uses the context
   # class timestamps to determine how long to wait before reading from the
   # replica.
-  #
+    config.action_mailer.default_url_options = {host: 'userfriendlyapp.herokuapp.com/'}
+
   # By default Rails will store a last write timestamp in the session. The
   # DatabaseSelector middleware is designed as such you can define your own
   # strategy for connection switching and pass that into the middleware through
